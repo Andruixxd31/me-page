dffbd7967d6006da14e79f344c54c5763ea783c5
Andruixxd31 <andruix93@gmail.com>
2020-04-08 20:07:20 -0500

    created the app with create-reactapp

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+//Here I will export the components that are part of the application
+//This app will render the components
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <div className="App-header">
+            <h1>Hello</h1>
+            <h1>Welcome to mePage</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
